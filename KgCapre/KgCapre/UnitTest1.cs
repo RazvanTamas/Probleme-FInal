fc805d6c5d518a78ebb8b3e07d74e728716f15a5
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-09-26 10:13:04 +0300

    Calculates the amount of food a given number of goats eat in a given number of days.

---
+++ KgCapre/KgCapre/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KilogrameManancaCaprele
+{
+    [TestClass]
+    public class TestKgMancare
+    {
+        [TestMethod]
+        public void TestKgManancaCaprele1()
+        {
+            double kg = Kilograme(10, 5, 200, 5, 2);
+            Assert.AreEqual(40, kg);
+        }
+        [TestMethod]
+        public void TestKgManancaCaprele2()
+        {
+            double kg = Kilograme(5, 10, 100, 8, 12);
+            Assert.AreEqual(192, kg);
+        }
+
+        double Kilograme(double nrcapre1, double zile1, double kg1, double nrcapre2, double zile2)
+        {
+
+            double kg1capra1zi = kg1 / (nrcapre1 * zile1);
+            double kg2 = zile2 * nrcapre2 * kg1capra1zi;
+
+            return kg2;
+
+        }
+
+    }
+}
+
