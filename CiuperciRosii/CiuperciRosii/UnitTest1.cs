a90b1df613def18273418421a9d72d5b449fee67
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-09-26 10:17:37 +0300

    Calculates the number of red mushrooms when the red ones are x times more than the white ones and the total number of mushrooms is given.

---
+++ CiuperciRosii/CiuperciRosii/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CiuperciRosii
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCiuperci1()
+        {
+           int nrciupercirosii = ciupercirosii(33,10);
+            Assert.AreEqual(30, nrciupercirosii);
+        }
+        [TestMethod]
+        public void TestCiuperci2()
+        {
+            int nrciupercirosii = ciupercirosii(30, 4);
+            Assert.AreEqual(24, nrciupercirosii);
+        }
+        int ciupercirosii(int n,int x)
+        {
+           int nrciupercialbe = n / (x + 1);
+           int nrciupercir = nrciupercialbe * x;
+            return nrciupercir;
+        }
+    }
+}
