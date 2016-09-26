98e7286422219776b5b130f0a44a2c24371d8c2a
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-09-26 10:18:51 +0300

    Calculates the distance made by a bird flying back and forth between two moving trains in the amount of time before they meet.

---
+++ DistantaPasare/DistantaPasare/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DistantaPasare
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDistPasare1()
+        {
+            Double Dist = DistPasare(100, 10);
+            Assert.AreEqual(112.5, Dist);
+        }
+        [TestMethod]
+        public void TestDistPasare2()
+        {
+            Double Dist = DistPasare(187, 25);
+            Assert.AreEqual(210.375, Dist);
+        }
+        double DistPasare(double distInit,double x)
+        {
+            double distantaPornPasare = distInit * 3 / 4;
+            double TimpIntalnireTrenuri = distantaPornPasare / (2 * x);
+            double VitPasare = 3 * x;
+                        ///Viteza pasarii este de 2*x la ca se adauga si viteza trenului catre care se indreapta;         
+            double DistantaFinalaPasare = TimpIntalnireTrenuri * VitPasare;
+            return DistantaFinalaPasare;
+        }
+    
+    }
+}
