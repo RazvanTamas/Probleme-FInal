c5c853a749dd17d861a411a5bd5a440216279221
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-09-27 01:45:29 +0300

    Calculate daytime taxi fare for a medium distance

--- ConditionalInstructions/TaxiFare/TaxiFareTest.cs
+++ ConditionalInstructions/TaxiFare/TaxiFareTest.cs
@@ -11,9 +11,15 @@ namespace TaxiFare
         {
             Assert.AreEqual(10, CalculateFare(2, 10)); 
         }
+        [TestMethod]
+        public void DaytimeFareFor25Km()
+        {
+            Assert.AreEqual(200, CalculateFare(25, 12));
+        }
         decimal CalculateFare(int distance,int hour)
         {
-            return distance*5;
+            decimal pricePerKm = distance > 20 ? 8 : 5;
+            return distance*pricePerKm;
         }
     }
 }
