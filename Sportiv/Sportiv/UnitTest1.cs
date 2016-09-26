68408eb80da2a8dca34f29079cd4559aa567f0ea
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-09-26 10:16:17 +0300

    Calculates the number of repetitions made by an athlete in a given number of rounds.

---
+++ Sportiv/Sportiv/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sportiv
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestNrRepetitii1()
+        {
+            double NrRepetitii = RepetitiiSportiv(10);
+            Assert.AreEqual(100, NrRepetitii);
+        }
+        [TestMethod]
+        public void TestNrRepetitii2()
+        {
+            double NrRepetitii = RepetitiiSportiv(7);
+            Assert.AreEqual(49, NrRepetitii);
+        }
+
+        double RepetitiiSportiv(double n)
+        {
+            Double repetitii = Math.Pow(n, 2);
+            // (n(n+1)/2)*2 -n => n(n+1)-n => n*n;          
+            return repetitii;
+        }
+    }
+}
