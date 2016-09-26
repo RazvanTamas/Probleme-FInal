f528d18656ebd9a06ea6552d01d6b926b380be56
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-09-26 10:13:49 +0300

    Calculates the number of stone bricks needed to pave a plaza.

---
+++ Pavaj/Pavaj/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pavaj
+{
+    [TestClass]
+    public class Testpavaj
+    {
+
+
+        [TestMethod]
+        public void TestPtPiata6x6sipiatra4x4()
+        {
+            int numar = nrbucatipietre(6, 6, 4);
+            Assert.AreEqual(4, numar);
+
+        }
+        [TestMethod]
+        public void TestPtPiata9x9sipiatra4x4()
+        {
+            int numar = nrbucatipietre(9, 9, 4);
+            Assert.AreEqual(9, numar);
+
+        }
+        [TestMethod]
+        public void TestPtPiata9x6sipiatra2x2()
+        {
+            int numar = nrbucatipietre(9, 6, 2);
+            Assert.AreEqual(15, numar);
+
+        }
+        int nrbucatipietre(double m,double n,double a)
+        {
+            
+            int numar1 =(int) Math.Ceiling(m/a);
+
+            int numar2 = (int)Math.Ceiling(n/a); 
+
+            int numar = numar1 * numar2; 
+
+                 
+               
+       
+
+            return numar;
+        }
+    }
+
+}
