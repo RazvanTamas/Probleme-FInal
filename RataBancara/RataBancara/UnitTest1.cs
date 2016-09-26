108ecd8248dbdf4dfa950ae4355bce1b7c76d056
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-09-26 10:15:41 +0300

    Calculates the bank rate of a certain month when the withdrawn value,interest rate and length of the bankrate are known.

---
+++ RataBancara/RataBancara/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RataBancara
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDobandaLuna1()         
+        {
+            double RataLuna1 = RataBanca(40000, 7.57, 240, 1);
+            Assert.AreEqual(419.00, RataLuna1);
+        }
+        [TestMethod]
+        public void TestDobandaLuna2()
+        {
+            double RataLuna2 = RataBanca(40000, 7.57, 240, 2);
+            Assert.AreEqual(417.95, RataLuna2);
+        }
+        [TestMethod]
+        public void TestDobandaLunaMartieAnul4()
+        {
+            double RataLunaMartieAn4 = RataBanca(40000, 7.57, 240, 39);
+            Assert.AreEqual(379.05, RataLunaMartieAn4);
+        }
+        double RataBanca(double Suma,double ProcentDob,int NrLuni,int LunaCurenta)
+        {
+            double RataLunaraFaraDobanda = Suma / NrLuni;
+            double SumaRamasa = Suma - (LunaCurenta - 1)*RataLunaraFaraDobanda;
+            double ProcentDobandaPeLuna = (ProcentDob / 12)/100;
+            double RataLunara = RataLunaraFaraDobanda + SumaRamasa * ProcentDobandaPeLuna;
+            return Math.Round(RataLunara,2);
+        }
+        
+    }
+}
