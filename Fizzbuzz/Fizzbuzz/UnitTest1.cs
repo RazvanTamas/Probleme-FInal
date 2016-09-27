24acae63902a8cd05e24eeeb1df8968c2d2824f5
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-09-27 04:25:14 +0300

    Add empty project FizzBuzz

---
+++ Fizzbuzz/Fizzbuzz/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fizzbuzz
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        string CalculateFizzBuzz(double n)
+        {
+            return "fizz";
+        }
+    }
+}
