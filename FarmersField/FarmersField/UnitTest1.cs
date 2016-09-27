9b370db1ead99913c12f2ef0ffeb945d4dcfc12c
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-09-27 03:40:41 +0300

    Add empty project for farmer's initial surface

---
+++ FarmersField/FarmersField/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//Un fermier are un teren patrat.Pentru a-si extinge suprafata a mai cumparat o parcela alaturata.
+//Noua parcela are aceeasi lungime si are o latime de 230m.Acum fermierul are 770 000mp.
+//Ce dimensiune avea initial terenul?
+namespace FarmersField
+{
+    [TestClass]
+    public class FarmersFieldTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+        decimal CalculateInitialSurface()
+        {
+
+        }
+    }
+}
