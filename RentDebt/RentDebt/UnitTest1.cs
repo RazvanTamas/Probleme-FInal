6d526117606cecd04628322d43f36483177f5917
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-09-27 04:43:09 +0300

    Added new empty project RentDebt

---
+++ RentDebt/RentDebt/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RentDebt
+{
+    [TestClass]
+    public class RentDebtTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
