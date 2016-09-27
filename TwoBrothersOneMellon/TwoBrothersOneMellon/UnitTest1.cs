805542194c4ebc60f6433a67d4a836f3c754e992
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-09-27 03:28:08 +0300

    Calculate if the two brothers have even parts of the mellon

---
+++ TwoBrothersOneMellon/TwoBrothersOneMellon/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//Doi prieteni au cumparat un pepene de x kg, si doresc sa il imparta.
+//Singura lor dorinca e ca fiecare sa primeasca un nr par de kg din pepene.
+//E ok si daca nu primesc aceeasi cantitate.
+//Scrie un program care raspunde cu DA sau NU(in fct de dorinta);
+namespace TwoBrothersOneMellon
+{
+    [TestClass]
+    public class TwoBrothersOneMellonTest
+    {
+        [TestMethod]
+        public void DividibleMellonTest()
+        {
+            Assert.AreEqual("Nu", AreTheySatisfied(3));
+        }
+        string AreTheySatisfied(int weightOfMelon)
+        {
+            for (double i = 1; i < weightOfMelon; i++)
+                for (double j = 1; j < weightOfMelon; j++)
+                    if ((i + j == weightOfMelon) && i / 2 == Math.Round(i / 2) && j / 2 == Math.Round(j / 2))
+                        return "Da";
+
+            return "Nu";
+
+
+             
+           
+        }
+    }
+}
