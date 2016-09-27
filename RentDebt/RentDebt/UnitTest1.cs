2422af9898c5bf3d6858aca022c1ea11e853fcae
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-09-27 05:01:49 +0300

    Calculate total rent for more than 30 days

--- RentDebt/RentDebt/UnitTest1.cs
+++ RentDebt/RentDebt/UnitTest1.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+//1-10 zile 2%/zi
+//11-30 zile 5%/zi
+//31-40 zile 10%/zi
 namespace RentDebt
 {
     [TestClass]
     public class RentDebtTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RentForMoreThan30Days()
         {
+            Assert.AreEqual(3300, CalculateTotalRent(100, 30));
+        }
+        decimal CalculateTotalRent(decimal rent, int day)
+        {
+            decimal interest = 10;
+            decimal debtPerDay = rent*interest/100;
+            decimal rentPerDay = rent + debtPerDay;
+            decimal totalRent = day * rentPerDay;
+            return totalRent;
         }
     }
 }
