9272a4336c3f78f24b8a0f2d4881d9774432e559
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-10-03 02:14:08 +0300

    Started new project Columns which converts the column number to the apropiate letters.

---
+++ Columns/Columns/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Columns
+{
+    [TestClass]
+    public class ColumsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("AZ", ConvertNumber(52));        
+        }
+        string ConvertNumber(int number)
+        {
+            return "AZ";
+        }
+    }
+}
