274c1f819b3c2fcca585d1bec2a3ec3ead7d548e
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-10-03 00:14:25 +0300

    Calculate if the sentence is a pangram

---
+++ Panagram/Panagram/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Panagram
+{
+    [TestClass]
+    public class PanagramTest
+    {
+        [TestMethod]
+        public void TestParagramCorrect()
+        {
+            CollectionAssert.IsSubsetOf(new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' }, CalculateIfParagram("the quick brown fox jumps over the lazy dog brown"));
+        }
+        char[] CalculateIfParagram(string sentence)
+        {
+            char[] SentenceChars = sentence.ToCharArray();
+            return SentenceChars;
+        }
+    }
+}
