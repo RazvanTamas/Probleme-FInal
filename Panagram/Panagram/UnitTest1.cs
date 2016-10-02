00a8d5fcee0a1cbd76fc2497bef37e7eed1cc75d
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-10-03 00:24:18 +0300

    Added function to change all letters to lowercase.

--- Panagram/Panagram/UnitTest1.cs
+++ Panagram/Panagram/UnitTest1.cs
@@ -9,12 +9,18 @@ namespace Panagram
         [TestMethod]
         public void TestParagramCorrect()
         {
-            CollectionAssert.IsSubsetOf(new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' }, CalculateIfParagram("the quick brown fox jumps over the lazy dog brown"));
+            CollectionAssert.IsSubsetOf(new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' }, CalculateIfParagram("The quick brown fox jumps over the LAZY dog brown"));
         }
         char[] CalculateIfParagram(string sentence)
         {
-            char[] SentenceChars = sentence.ToCharArray();
-            return SentenceChars;
+            char[] sentenceChars = sentence.ToCharArray();
+            for (int i = 0; i < sentenceChars.Length; i++)
+            {
+                char letter = sentenceChars[i];
+                if (char.IsUpper(letter))
+                    sentenceChars[i] = char.ToLower(letter);                
+            }
+            return sentenceChars;
         }
     }
 }
