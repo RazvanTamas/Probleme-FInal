f58aae4bb962174f050530a252e04f83a3f20741
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-10-03 01:46:03 +0300

    Started new project for calculating the "x"th nr which has it's cube end in 888;

---
+++ CubeWith888/CubeWith888/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CubeWith888
+{
+    [TestClass]
+    public class CubeWith888Test
+    {
+        [TestMethod]
+        public void TestFirstNumber()
+        {
+            Assert.AreEqual(192, CalculateNumber(1));
+        }
+        int CalculateNumber(int n)
+        {
+            return 192;
+        }
+    }
+}
