e33b73bd8eea82d18d28a8a73a43c788e6fed2e8
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-10-02 23:24:23 +0300

    Begin new project PefixTest which returns "aaa"

---
+++ PrefixTest/PrefixTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrefixTest
+{
+    [TestClass]
+    public class PrefixTest
+    {
+        [TestMethod]
+        public void PrefixTest1()
+        {
+            Assert.AreEqual("aaa", Prefix("aaab", "aaaabbaa"));
+        }
+        string Prefix(string word1,string word2)
+        {
+            return "aaa";
+        }
+    }
+}
