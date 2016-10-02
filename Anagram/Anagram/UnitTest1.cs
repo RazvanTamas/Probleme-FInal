6a487e8a7f1920dca1a5ff475058cf17a3b6c2b3
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-10-03 00:30:12 +0300

    Started new project for calculating number of posible anagrams of a given word

---
+++ Anagram/Anagram/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Anagram
+{
+    [TestClass]
+    public class AnagramTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(9, CalculateAnagramNr("abc"));
+        }
+        int CalculateAnagramNr(string word)
+        {
+            return 9;
+        }
+    }
+}
