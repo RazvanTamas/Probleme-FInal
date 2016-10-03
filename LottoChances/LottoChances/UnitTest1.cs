7e9bb6e2857002d9e47c516c7abae4cca007e8ae
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-10-03 04:37:55 +0300

    Started new project LottoChances

---
+++ LottoChances/LottoChances/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LottoChances
+{
+    [TestClass]
+    public class LottoChancesTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0.0000000715112384201851626194m, CalculateChances(6, 49));
+        }
+        decimal CalculateChances(int favNumbers,int allNumbers)
+        {
+            return 0.0000000715112384201851626194m;
+
+        }
+    }
+}
