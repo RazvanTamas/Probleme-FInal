412e0bc8bc8f489928c08e292ad6d110ccaf5778
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-10-03 17:26:46 +0300

    Extracted Lengths function

--- PrefixTest/PrefixTest/UnitTest1.cs
+++ PrefixTest/PrefixTest/UnitTest1.cs
@@ -12,30 +12,42 @@ namespace PrefixTest
             Assert.AreEqual("Ana",CalculatePrefix("AnaAre", "AnaAreMere"));
         }
         string CalculatePrefix(string firstWord, string secondWord)
-        {           
+        {
             int i = 0;
-            string prefix = null;           
-            while (firstWord[i] == secondWord[i])
+            int minLength = 1;
+            string prefix = null;
+            minLength = Lengths(firstWord, secondWord);
+
+            while (firstWord[i] == secondWord[i] && i <= minLength)
             {
                 prefix += firstWord[i];
-                i++;               
+                i++;
             }
             return prefix;
-            
-           // char[] prefixArray = PutPrefixInArray(firstWord, i);
-          //  string prefix = new string(prefixArray, 0, i);
-          //  return prefix;
+
+            // char[] prefixArray = PutPrefixInArray(firstWord, i);
+            //  string prefix = new string(prefixArray, 0, i);
+            //  return prefix;
         }
 
-       // private static char[] PutPrefixInArray(string firstWord, int i)
-      //  {
-       //     char[] prefixArray = new char[i + 1];
-         //   for (int j = 0; j <= i; j++)
+        private static int Lengths(string firstWord, string secondWord)
+        {
+            int minLength;
+            if (firstWord.Length <= secondWord.Length)
+                minLength = firstWord.Length;
+            else minLength = secondWord.Length;
+            return minLength;
+        }
+
+        // private static char[] PutPrefixInArray(string firstWord, int i)
+        //  {
+        //     char[] prefixArray = new char[i + 1];
+        //   for (int j = 0; j <= i; j++)
         //    {
         //        prefixArray[j] = firstWord[j];
         //    }
 
-         //   return prefixArray;
-       // }
+        //   return prefixArray;
+        // }
     }
 }
