da1701a9df967afb38e7a61bc892cf5dd7f6d414
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-10-03 04:15:41 +0300

    Started project DaysTillWeMeet in which we calculate the minimum number of days needed to meet.

---
+++ DaysTillWeMeet/DaysTillWeMeet/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DaysTillWeMeet
+{
+    [TestClass]
+    public class DaysTillWeMeetTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(12, CalculateDaysWeMeet(4, 6));
+        }
+        int CalculateDaysWeMeet(int days1,int days2)
+        {
+            return 12;
+        }
+    }
+}
