3070e810654ad4d636636ba58b43471b27d08463
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-10-03 04:09:29 +0300

    Started new project to calculate maximum number of squares on a chess table.

---
+++ ChessSquares/ChessSquares/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessSquares
+{
+    [TestClass]
+    public class ChessSquaresTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(204, CalculateSquares(8));
+        }
+        int CalculateSquares(int n)
+        {
+            return 204;
+        }
+    }
+}
