3fd9be8325d8d61896e4c81524f5047784bdbbe2
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-10-03 03:59:47 +0300

    Calculate the correct letter combination for a number of excel columns.

--- Columns/Columns/UnitTest1.cs
+++ Columns/Columns/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Columns
 {
@@ -13,7 +14,26 @@ namespace Columns
         }
         string ConvertNumber(int number)
         {
-            return "AZ";
+            char[] alphabet = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            int pointer = 0;
+            List<char> LettersOfColumnNumber = new List<char>();
+            while (number >0)
+            {
+                pointer = (number-1)%26 ;              
+                LettersOfColumnNumber.Add(alphabet[pointer]);
+                number = (number-pointer)/26;
+               
+            }
+            string convertedNumber = string.Join("", LettersOfColumnNumber.ToArray());
+            char[] correctOrder = new char[convertedNumber.Length];
+            int j = 0;
+            for (int i = convertedNumber.Length - 1; i >= 0; i--)
+            {
+                correctOrder[j] = convertedNumber[i];
+                j++;
+            }
+            string correctOrderNumber = new string(correctOrder);
+            return correctOrderNumber;
         }
     }
 }
