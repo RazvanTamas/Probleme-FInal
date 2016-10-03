24a9635fee957af9625f11d0f804fdde19f1eb9f
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-10-03 04:44:45 +0300

    Calculates lotto chances

--- LottoChances/LottoChances/UnitTest1.cs
+++ LottoChances/LottoChances/UnitTest1.cs
@@ -7,14 +7,25 @@ namespace LottoChances
     public class LottoChancesTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestFor6()
         {
             Assert.AreEqual(0.0000000715112384201851626194m, CalculateChances(6, 49));
         }
+        [TestMethod]
+        public void TestFor5()
+        {
+            Assert.AreEqual(0.0000005244157484146911925424m, CalculateChances(5, 49));
+        }
         decimal CalculateChances(int favNumbers,int allNumbers)
         {
-            return 0.0000000715112384201851626194m;
-
+            decimal favNumbersDec = Convert.ToDecimal(favNumbers);            
+            decimal probability = 1;
+            for(decimal i = 1; i <= favNumbersDec; i++)
+            {
+                probability = probability * (i / allNumbers);
+                allNumbers -= 1;
+            }
+            return probability;
         }
     }
 }
