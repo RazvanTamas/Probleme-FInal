b7f327ef8ce078d8e1eaaa1011c7c00569a7d5ca
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-10-03 17:02:14 +0300

    Calculate letters of column with ASCII

--- Columns/Columns/UnitTest1.cs
+++ Columns/Columns/UnitTest1.cs
@@ -12,40 +12,18 @@ namespace Columns
         {
             Assert.AreEqual("AZ", ConvertNumber(52));        
         }
+   
         string ConvertNumber(int number)
         {
-            char[] alphabet = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-            int pointer = 0;
-            List<char> LettersOfColumnNumber = new List<char>();
-            PutLettersInList(ref number, alphabet, ref pointer, LettersOfColumnNumber);
-            char[] correctOrder = ExtractLettersFromListInOrder(LettersOfColumnNumber);
-            string correctOrderNumber = new string(correctOrder);
-            return correctOrderNumber;
-        }
-
-        private static char[] ExtractLettersFromListInOrder(List<char> LettersOfColumnNumber)
-        {
-            string convertedNumber = string.Join("", LettersOfColumnNumber.ToArray());
-            char[] correctOrder = new char[convertedNumber.Length];
-            int j = 0;
-            for (int i = convertedNumber.Length - 1; i >= 0; i--)
-            {
-                correctOrder[j] = convertedNumber[i];
-                j++;
-            }
-
-            return correctOrder;
-        }
-
-        private static void PutLettersInList(ref int number, char[] alphabet, ref int pointer, List<char> LettersOfColumnNumber)
-        {
+            string convertedNumber = string.Empty;
             while (number > 0)
             {
-                pointer = (number - 1) % 26;
-                LettersOfColumnNumber.Add(alphabet[pointer]);
-                number = (number - pointer) / 26;
+                int change = (number - 1) % 26;
+                convertedNumber = Convert.ToChar(65+change).ToString()+convertedNumber;                               
+                number = (number - change) / 26;
 
             }
+            return convertedNumber;
         }
     }
 }
