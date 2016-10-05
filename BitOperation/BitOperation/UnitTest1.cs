0caaac0b91f1fda6bbaf21cedf493555de9d4e99
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-10-06 00:25:46 +0300

    Extracted InvertArray function

--- BitOperation/BitOperation/UnitTest1.cs
+++ BitOperation/BitOperation/UnitTest1.cs
@@ -15,16 +15,23 @@ namespace BitOperation
         }
         string CalculateNumberInBase(int number,int wantedBase)
         {
-                  
+
             byte[] biteArray = new byte[0];
             string numberInBits = string.Empty;
             while (number != 0)
             {
                 Array.Resize(ref biteArray, biteArray.Length + 1);
-                biteArray[biteArray.Length-1] = (byte)(number % wantedBase);
+                biteArray[biteArray.Length - 1] = (byte)(number % wantedBase);
                 number = number / wantedBase;
 
             }
+            numberInBits = InvertArray(biteArray, numberInBits);
+
+            return numberInBits;
+        }
+
+        private static string InvertArray(byte[] biteArray, string numberInBits)
+        {
             for (int i = biteArray.Length - 1; i >= 0; i--)
             {
                 numberInBits += biteArray[i];
@@ -32,7 +39,5 @@ namespace BitOperation
 
             return numberInBits;
         }
-
-
     }
 }
