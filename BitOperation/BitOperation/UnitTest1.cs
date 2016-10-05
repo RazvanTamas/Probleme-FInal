cbadf7109595f06f979a0df2dbb8c8ebc56f1328
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-10-05 09:59:50 +0300

    Started new project BitOperation

---
+++ BitOperation/BitOperation/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitOperation
+{
+    [TestClass]
+    public class BitOperationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("10001110", CalculateBase(142));
+        }
+        string CalculateBase(int number)
+        {
+            string numberInBase = "10001110";
+            return numberInBase;
+        }
+
+    }
+}
