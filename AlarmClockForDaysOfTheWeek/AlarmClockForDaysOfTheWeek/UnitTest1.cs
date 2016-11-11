1a52e9f232754bc4ee102f8c22a8595e779c7036
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-11-11 12:02:59 +0200

    Alarm clock with an array which holds alarm hour for each day

--- AlarmClockForDaysOfTheWeek/AlarmClockForDaysOfTheWeek/UnitTest1.cs
+++ AlarmClockForDaysOfTheWeek/AlarmClockForDaysOfTheWeek/UnitTest1.cs
@@ -12,32 +12,26 @@ namespace AlarmClockForDaysOfTheWeek
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(true, IsAlarm(5,8,8,10,1,8));
+            Assert.AreEqual(true,IsAlarmAtDayAndHour(new int[] { 10,10,10,8,8,7,7},"Saturday",7));
         }
         [Flags]
         enum DaysOfTheWeek
         {
             None=0,
-            Monday = 1 << 0,
-            Tuesday = 1 << 1,
-            Wednesday = 1 << 2,
-            Thursday = 1 << 3,
-            Friday = 1 << 4,
-            Saturday = 1 << 5,
-            Sunday = 1 << 6 
-        }
-        bool IsAlarm(byte alarmOneDays,int alarmOneHour,byte alarmTwoDays,int alarmTwoHour,byte givenDay,int givenHour)
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7 
+        }      
+        bool IsAlarmAtDayAndHour(int[] alarmHourForEachDay,string dayToCheck,int hourToCheck)
         {
-            bool alarm = false;
-            if ((alarmOneDays & givenDay) == givenDay && alarmOneHour == givenHour)
-            {
-                return alarm = true;
-            }
-            else if ((alarmTwoDays & givenDay) == givenDay && alarmTwoHour == givenHour)
-            {
-                return alarm = true;
-            }
-            else return alarm = false;           
+            DaysOfTheWeek dayNumber;
+            Enum.TryParse<DaysOfTheWeek>(dayToCheck, out dayNumber);
+            if (alarmHourForEachDay[(int)dayNumber-1] == hourToCheck) return true;
+            else return false;                      
         }
     }
     
