43b55aa50b1eefb2b09421f32a8b842e989b3af7
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-11-14 17:57:38 +0200

    Generate Digit chars and rest string

--- PasswordGenerator/PasswordGenerator/UnitTest1.cs
+++ PasswordGenerator/PasswordGenerator/UnitTest1.cs
@@ -19,7 +19,7 @@ namespace PasswordGenerator
         [TestMethod]
         public void TestUpperCaseChars()
         {
-            var options = new PasswordOptions("UpperCase", 5);
+            var options = new PasswordOptions[] { new PasswordOptions("UpperCase", 5) };
             var result = PasswordGenerator(8, options);
             int numberOfUpperCaseChars = 0;
             for (int i = 0; i < result.Length; i++)
@@ -28,16 +28,51 @@ namespace PasswordGenerator
             }
             Assert.AreEqual(5,numberOfUpperCaseChars);
         }
+        [TestMethod]
+        public void TestDigitChars()
+        {
+            var options = new PasswordOptions[] { new PasswordOptions("UpperCase", 3), new PasswordOptions("Digits", 5) };
+            var result = PasswordGenerator(8, options);
+            int numberOfDigitChars = 0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] >= '0' && result[i] <= '9') numberOfDigitChars++;
+            }
+            Assert.AreEqual(5, numberOfDigitChars);
+        }
        
-        string PasswordGenerator(int passwordLength,PasswordOptions options)
+        string PasswordGenerator(int passwordLength,PasswordOptions[] options)
         {
             Random rand = new Random();
-            string password = string.Empty; 
-            if (options.option == "UpperCase")
+            string password = string.Empty;
+            string rest = string.Empty;
+            for (int i = 0; i < options.Length; i++)
             {
-                for (int i=0;i<options.numberOfChars;i++) password += (char)('A' + rand.Next(0, 26));
-            }          
+                if (options[i].option == "UpperCase" && options[i].numberOfChars > 0)
+                    GenerateUpperCaseLowerCaseAndDigitChars(ref password, options[i].numberOfChars, 'A', 26, rand);
+                else if (options[i].option == "UpperCase" && options[i].numberOfChars == 0)
+                    GenerateRest(ref rest, 'A', 26, rand);
+                else if (options[i].option == "Digits" && options[i].numberOfChars > 0)
+                    GenerateUpperCaseLowerCaseAndDigitChars(ref password, options[i].numberOfChars, '0', 9, rand);
+                else if (options[i].option == "Digits" && options[i].numberOfChars == 0)
+                    GenerateRest(ref rest, '0', 9, rand);
+            }
+               
             return password;   
+        }
+        public void GenerateUpperCaseLowerCaseAndDigitChars(ref string password,int neededChars,char firstChar,int range,Random rand)
+        {
+            for (int i = 0; i < neededChars; i++)
+            {
+                password += (char)(firstChar + rand.Next(0, range));
+            }
+        }
+        public void GenerateRest(ref string rest, char firstChar, int range, Random rand)
+        {
+            for (int i = 0; i < range; i++)
+            {
+                rest += (char)(firstChar + i);
+            }
         }
     }  
 
