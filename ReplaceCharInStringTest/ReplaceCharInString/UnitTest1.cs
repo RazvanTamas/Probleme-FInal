84d7a5cd3970f4d0e8ef3b5b3d7dd73c0c2705d5
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-11-28 22:47:35 +0200

    Replace char in string with other string with and without recursion function

---
+++ ReplaceCharInStringTest/ReplaceCharInString/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReplaceCharInString
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("aaaaaaStringAddedaaaa",ReplaceCharInString("aaaaaabaaaa",'b',"StringAdded"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual("aaaaaaStringAddedaaaa", ReplaceCharInStringWithRecursion("aaaaaabaaaa", 'b', "StringAdded"));
+        }
+
+        string ReplaceCharInString(string stringGiven,char charToReplace,string stringToAddInPlaceOfChar)
+        {
+            string charToReplaceConvertedToString = charToReplace.ToString();
+            stringGiven = stringGiven.Replace(charToReplaceConvertedToString, stringToAddInPlaceOfChar);
+            return stringGiven;
+        }
+
+        string ReplaceCharInStringWithRecursion(string stringGiven,char charToReplace,string stringToAddInPlaceOfChar)
+        {
+            string newString = "";
+            int i = stringGiven.Length - 1;
+            newString = RecursiveFunction(stringGiven, charToReplace, stringToAddInPlaceOfChar, i);
+            return newString;
+        }
+        string RecursiveFunction(string stringGiven,char charToReplace,string stringToAddInPlaceOfChar,int i)
+        {
+            if (i > 0)
+            {
+                if (stringGiven[i] == charToReplace)
+                    return RecursiveFunction(stringGiven, charToReplace, stringToAddInPlaceOfChar, i-1)+ stringToAddInPlaceOfChar;
+                else
+                    return  RecursiveFunction(stringGiven, charToReplace, stringToAddInPlaceOfChar, i-1)+stringGiven[i];
+            }
+            else return stringGiven[i].ToString();
+        }
+    }
+}
