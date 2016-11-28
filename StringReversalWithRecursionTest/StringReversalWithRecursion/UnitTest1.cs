55f6689575eacc464bd61fe76a45584ee83cfb4e
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-11-28 21:44:55 +0200

    Reverse string with length <=1

---
+++ StringReversalWithRecursionTest/StringReversalWithRecursion/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringReversalWithRecursion
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("a", ReverseString("a"));
+        }
+
+        string ReverseString(string stringForReversion)
+        {
+            if (stringForReversion.Length <= 1) return stringForReversion;           
+            return stringForReversion;
+        }
+    }
+}
