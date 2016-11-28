e951070f56db380e9c21eb5742a2778885967104
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-11-28 17:39:35 +0200

    Calculate the fibonacci sequence for the first 2 numbers

---
+++ FibonacciTest/Fibonacci/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fibonacci
+{
+    [TestClass]
+    public class FibonacciTest
+    {
+        [TestMethod]
+        public void FibonacciTestForZeroNumbers()
+        {
+            Assert.AreEqual(0, CalculateFibonacci(0));
+        }
+        [TestMethod]
+        public void FibonacciTestForOneNumber()
+        {
+            Assert.AreEqual(0, CalculateFibonacci(1));
+        }
+        [TestMethod]
+        public void FibonacciTestForSecondNumber()
+        {
+            Assert.AreEqual(1, CalculateFibonacci(2));
+        }
+        int CalculateFibonacci(int k)
+        {
+            int firstNumber = 0;
+            int secondNumber = 1;
+            if (k ==2) return (secondNumber);
+            return firstNumber;
+        }
+    }   
+}
