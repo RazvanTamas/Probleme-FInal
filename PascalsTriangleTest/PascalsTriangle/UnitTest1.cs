3dd5244fca5e139853f2855f5b5b89e10bd5f96d
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-11-29 20:30:52 +0200

    first row for the triangle

---
+++ PascalsTriangleTest/PascalsTriangle/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PascalsTriangle
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestForRowNumberOne()
+        {
+            Assert.AreEqual("1", GenerateRowFromPascalsTriangle(1));
+        }
+
+        string GenerateRowFromPascalsTriangle(int rowNumber)
+        {
+            int rowNumberInArrays = rowNumber - 1;
+            string[] pascalRow = new string[rowNumber];
+            if ((rowNumberInArrays) == 0)
+            {
+                string currentRow = string.Empty;
+                currentRow += '1';
+                pascalRow[rowNumberInArrays] = currentRow;
+            }
+            return pascalRow[rowNumberInArrays];
+        }
+    }
+}
