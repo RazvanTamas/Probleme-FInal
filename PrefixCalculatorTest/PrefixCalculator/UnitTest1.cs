087831ef8df999f79bebe0f77f16944308892858
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-12-04 23:25:22 +0200

    Separate numbers and symbols in different strings

---
+++ PrefixCalculatorTest/PrefixCalculator/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrefixCalculator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("+", PutNumbersAndSymbolsInDifferentStrings("+ 12 44"));
+        }
+            
+        string PutNumbersAndSymbolsInDifferentStrings(string givenExpression)
+        {
+            string numbers = string.Empty;
+            string operations = string.Empty;
+            FindNumbersAndSymbols(0, givenExpression,ref numbers,ref operations);
+            return operations;                 
+        } 
+        public static void FindNumbersAndSymbols(int i,string givenExpression,ref string numbers,ref string operations)
+        {
+            string operationsCollection = "+-*/";
+            if (i < givenExpression.Length)
+            {            
+                if (operationsCollection.Contains(givenExpression[i]))
+                    operations += givenExpression[i];
+                else
+                    numbers += givenExpression[i];
+                FindNumbersAndSymbols(i + 1, givenExpression,ref numbers,ref operations);
+            }          
+        }        
+    }
+}
