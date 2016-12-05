6aea944e8b4b8d3d5d5775819a3b8c2ff726efb6
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-12-05 02:06:55 +0200

    Towers of hanoi for 2 disks

---
+++ MovesForHanoiTowersTest/MovesForHanoiTowers/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovesForHanoiTowers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestForTwoTowers()
+        {
+            Assert.AreEqual("AB AC BC ", ShowMovesForHanoiTowers(2));
+        }
+        string ShowMovesForHanoiTowers(int numberOfDisks)
+        {
+            string moves = string.Empty;
+            MovesForTwoTowers(numberOfDisks, 'A', 'B', 'C',ref moves);
+            return moves;
+        }
+        public void MovesForTwoTowers(int numberOfDisks,char sourceDisk,char helperDisk,char destinationDisk,ref string moves)
+        {
+            if (numberOfDisks > 0)
+            {
+                MovesForTwoTowers(numberOfDisks - 1, sourceDisk, destinationDisk, helperDisk,ref moves);              
+                moves += sourceDisk;
+                moves += destinationDisk;
+                moves += " ";
+                MovesForTwoTowers(numberOfDisks - 1, helperDisk, sourceDisk, destinationDisk,ref moves);
+            }
+
+        }
+    }
+}
