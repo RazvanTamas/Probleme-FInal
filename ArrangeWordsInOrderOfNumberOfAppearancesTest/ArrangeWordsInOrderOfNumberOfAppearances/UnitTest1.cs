c22d2bb9ac448744c4ff3b793fd7bf1a2d57bb5f
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2016-12-19 16:50:15 +0200

    Now we get the new string with the words in order but without symbols

--- ArrangeWordsInOrderOfNumberOfAppearancesTest/ArrangeWordsInOrderOfNumberOfAppearances/UnitTest1.cs
+++ ArrangeWordsInOrderOfNumberOfAppearancesTest/ArrangeWordsInOrderOfNumberOfAppearances/UnitTest1.cs
@@ -20,14 +20,25 @@ namespace ArrangeWordsInOrderOfNumberOfAppearances
         [TestMethod]
         public void ArrangeNumbersInOrderOfAppearances()
         {
-            CollectionAssert.AreEqual(new string[] { "three", "three", "three", "two", "two", "one" }, Main("one three two three three two"));
+            Assert.AreEqual("Three Three Three two two One", Main("Three One Three two Three two"));
         }
 
-        string [] Main(string givenText)
+        string  Main(string givenText)
         {
             var arrayOfWords = PutWordsInArray(givenText);
             ArrangeWordsInNumberOfAppearances(ref arrayOfWords, givenText);
-            return arrayOfWords;
+            string newString = string.Empty;          
+            newString = BuildNewString(ref newString, arrayOfWords, 0);
+            return newString.Substring(0, newString.Length - 1);
+
+        }
+        string BuildNewString(ref string newString,string [] arrayOfWords,int i)
+        {
+            if (i == arrayOfWords.Length)
+                return newString;
+            newString += arrayOfWords[i];
+            newString += " ";
+            return BuildNewString(ref newString, arrayOfWords, i + 1);
         }
 
         string [] PutWordsInArray(string givenText)
