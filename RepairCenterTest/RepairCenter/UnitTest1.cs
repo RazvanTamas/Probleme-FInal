607045b18a249c44f00d1d3aa0e572c43fe788db
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2017-01-27 07:13:22 +0200

    Started project RepairCenterTest

---
+++ RepairCenterTest/RepairCenter/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RepairCenter
+{
+    public struct RepairCase
+    {
+        public string caseName;
+        public string priority;
+        public RepairCase(string caseName,string priority)
+        {
+            this.caseName = caseName;
+            this.priority = priority;
+        }
+
+    }
+    [TestClass]
+    public class RepairCenterTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        public RepairCase[] SortCasesAccordingToPriority(RepairCase[] repairCases)
+        {
+            for(int i = 0; i < repairCases.Length; i++)
+            {
+                int x = 0;
+            }
+            return repairCases;
+        }
+    }
+}
