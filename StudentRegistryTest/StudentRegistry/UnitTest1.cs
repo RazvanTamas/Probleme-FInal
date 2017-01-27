0d2fee37d864fe07f59915bbe9044889c44ff0a5
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2017-01-27 08:16:20 +0200

    Started new project StudentRegistryTest

---
+++ StudentRegistryTest/StudentRegistry/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudentRegistry
+{
+    public struct Subject
+    {
+        public string subjectName;
+        public int[] grades;
+        public Subject(string subjectName, int[] grades)
+        {
+            this.subjectName = subjectName;
+            this.grades = grades;
+        }
+    }
+    public struct Student
+    {
+        public string studentName;
+        public Subject[] subjects;
+        public Student(string studentName, Subject[] subjects)
+        {
+            this.studentName = studentName;
+            this.subjects = subjects;
+        }
+    }
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
