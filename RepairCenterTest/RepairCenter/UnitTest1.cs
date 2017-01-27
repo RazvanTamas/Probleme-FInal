ff0a85be2ce680e29c4958285e20edc5f1bb1b35
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2017-01-27 07:43:42 +0200

    Added a function which swaps elements in the array, a function which returns an integer (based on the priority) and added the selection sorting code

--- RepairCenterTest/RepairCenter/UnitTest1.cs
+++ RepairCenterTest/RepairCenter/UnitTest1.cs
@@ -18,16 +18,46 @@ namespace RepairCenter
     public class RepairCenterTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestSortCasesAccordintToPriority()
         {
+            var cases = new RepairCase[] { new RepairCase("phone", "Medium"), new RepairCase("tv", "High"), new RepairCase("iPhone", "Low"), new RepairCase("Boombox", "Medium") };
+            CollectionAssert.AreEqual(new RepairCase[] { new RepairCase("iPhone", "Low"), new RepairCase("phone", "Medium"), new RepairCase("Boombox", "Medium"), new RepairCase("tv", "High") }, SortCasesAccordingToPriorityUsingSelection(cases));
         }
 
-        public RepairCase[] SortCasesAccordingToPriority(RepairCase[] repairCases)
+        public RepairCase[] SortCasesAccordingToPriorityUsingSelection(RepairCase[] repairCases)
         {
+            int k;
             for(int i = 0; i < repairCases.Length; i++)
             {
-                int x = 0;
+                k = i;
+                for (int j = i + 1; j < repairCases.Length; j++)
+                {
+                    if (ReturnCasePriorityInInteger(repairCases[j]) < ReturnCasePriorityInInteger(repairCases[k]))
+                        k = j;              
+                }
+                if (k != i)
+                    SwapCasesAccordingToPriority(ref repairCases, i, k);
             }
+            return repairCases;
+        }   
+
+        public int ReturnCasePriorityInInteger(RepairCase repairCase)
+        {
+            int i = 1;
+            if (repairCase.priority == "Medium")
+                i = 2;
+            if (repairCase.priority == "High")
+                i = 3;
+            return i;
+        }
+
+        public RepairCase[] SwapCasesAccordingToPriority(ref RepairCase[] repairCases,int i,int j)
+        {
+            var temp = repairCases[i];
+            repairCases[i].caseName = repairCases[j].caseName;
+            repairCases[i].priority = repairCases[j].priority;
+            repairCases[j].caseName = temp.caseName;
+            repairCases[j].priority = temp.priority;
             return repairCases;
         }
     }
