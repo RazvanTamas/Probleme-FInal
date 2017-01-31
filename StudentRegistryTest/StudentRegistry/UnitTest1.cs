511089cd586023c5183b880e16ba2e06692f62f7
DESKTOP-OPFBOHK\Shog <razvan_tamash@yahoo.com>
2017-01-31 06:42:31 +0200

    Added a binary search function for the student with a certain grade

--- StudentRegistryTest/StudentRegistry/UnitTest1.cs
+++ StudentRegistryTest/StudentRegistry/UnitTest1.cs
@@ -30,14 +30,14 @@ namespace StudentRegistry
         public void TestSortStudentsInAlphabeticalOrder()
         {
             var students = new Student[] { new Student("Andrew Wiggins", new Subject[] { new Subject("math", new int[] { 10, 8, 9 }), new Subject("English", new int[] { 9, 9, 8, 4 }), new Subject("Science", new int[] { 5, 4, 10, 10 }) }), new Student("Jabari Parker", new Subject[] { new Subject("Science", new int[] { 5, 5, 4, 7 }), new Subject("English", new int[] { 10, 9 }), new Subject("Math", new int[] { 6, 7, 4, 10 }) }), new Student("Giannis Antetokounmpo", new Subject[] { new Subject("English", new int[] { 8, 8, 9 }), new Subject("Math", new int[] { 9, 7, 8, 4 }), new Subject("Science", new int[] { 4, 9, 6, 8 }) }) };
-            Assert.AreEqual("Andrew Wiggins,Giannis Antetokounmpo,Jabari Parker",BuildStringWithStudentNames(SortStudentsInAlphabeticalOrderUsingSelection(ref students)));
+            Assert.AreEqual("Andrew Wiggins,Giannis Antetokounmpo,Jabari Parker", BuildStringWithStudentNames(SortStudentsInAlphabeticalOrderUsingSelection(ref students)));
         }
 
         [TestMethod]
         public void TestAverageGradeOfStudent()
         {
-            var student=new Student("Andrew Wiggins", new Subject[] { new Subject("math", new int[] { 10, 8, 9 }), new Subject("English", new int[] { 9, 9, 8, 4 }), new Subject("Science", new int[] { 5, 4, 10, 10 }) });
-            Assert.AreEqual(7.92m, AverageGeneralGradeForEachStudent(student)); 
+            var student = new Student("Andrew Wiggins", new Subject[] { new Subject("math", new int[] { 10, 8, 9 }), new Subject("English", new int[] { 9, 9, 8, 4 }), new Subject("Science", new int[] { 5, 4, 10, 10 }) });
+            Assert.AreEqual(7.92m, AverageGeneralGradeForEachStudent(student));
         }
 
         [TestMethod]
@@ -51,14 +51,14 @@ namespace StudentRegistry
         public void TestSeparateNames()
         {
             var student = new Student("Andrew Wiggins The Third", new Subject[] { new Subject("math", new int[] { 10, 8, 9 }), new Subject("English", new int[] { 9, 9, 8, 6 }), new Subject("Science", new int[] { 5, 7, 10, 10 }) });
-            CollectionAssert.AreEqual(new string[] { "Andrew", "Wiggins","The","Third" }, SeparateNamesInDifferentStrings(student));
+            CollectionAssert.AreEqual(new string[] { "Andrew", "Wiggins", "The", "Third" }, SeparateNamesInDifferentStrings(student));
         }
 
         [TestMethod]
         public void TestIfInAlphabeticalOrder()
         {
-            string[] studentOne = new string[] { "Joany","Darter" };
-            string[] studentTwo = new string[] { "John","Barter", "Misc" };
+            string[] studentOne = new string[] { "Joany", "Darter" };
+            string[] studentTwo = new string[] { "John", "Barter", "Misc" };
             Assert.AreEqual(true, CheckIfInAlphabeticalOrder(studentOne, studentTwo));
         }
 
@@ -84,29 +84,36 @@ namespace StudentRegistry
             Assert.AreEqual("Jabari Parker,Giannis Antetokounmpo,Andrew Wiggins", BuildStringWithStudentNames(SortStudentsBasedOnTheirAverageGradeUsingBubbleSorting(ref students)));
         }
 
+        [TestMethod]
+        public void TestFindAStudentWithACertainAverageGradeUsingBinarySearch()
+        {
+            var students = new Student[] { new Student("Andrew Wiggins", new Subject[] { new Subject("math", new int[] { 10, 8, 9 }), new Subject("English", new int[] { 9, 9, 8, 4 }), new Subject("Science", new int[] { 5, 4, 10, 10 }) }), new Student("Jabari Parker", new Subject[] { new Subject("Science", new int[] { 5, 5, 4, 7 }), new Subject("English", new int[] { 10, 9 }), new Subject("Math", new int[] { 6, 7, 4, 10 }) }), new Student("Giannis Antetokounmpo", new Subject[] { new Subject("English", new int[] { 8, 8, 9 }), new Subject("Math", new int[] { 9, 7, 8, 4 }), new Subject("Science", new int[] { 4, 9, 6, 8 }) }) };
+            Assert.AreEqual("Andrew Wiggins", FindStudentWithACertainGradeAverageUsingBinarySearch(students, 7.92m));
+        }
+
         decimal AverageGeneralGradeForEachStudent(Student student)
         {
             decimal[] averageGradeForEachSubject = new decimal[0];
-            decimal averageGrade = 0;      
+            decimal averageGrade = 0;
             for (int i = 0; i < student.subjects.Length; i++)
             {
                 AverageGradeForEachSubject(student, ref averageGradeForEachSubject, ref averageGrade, i);
             }
-            return Math.Round(averageGrade/student.subjects.Length,2, MidpointRounding.AwayFromZero);
+            return Math.Round(averageGrade / student.subjects.Length, 2, MidpointRounding.AwayFromZero);
         }
 
         private static void AverageGradeForEachSubject(Student student, ref decimal[] averageGradeForEachSubject, ref decimal averageGrade, int i)
-        {        
+        {
             for (int j = 0; j < student.subjects[i].grades.Length; j++)
             {
                 Array.Resize(ref averageGradeForEachSubject, averageGradeForEachSubject.Length + 1);
-                averageGradeForEachSubject[i] += student.subjects[i].grades[j];           
+                averageGradeForEachSubject[i] += student.subjects[i].grades[j];
             }
             averageGradeForEachSubject[i] = averageGradeForEachSubject[i] / student.subjects[i].grades.Length;
             averageGrade += averageGradeForEachSubject[i];
         }
 
-        int CountACertainGradeForEachStudent(Student student,int grade)
+        int CountACertainGradeForEachStudent(Student student, int grade)
         {
             int count = 0;
             for (int i = 0; i < student.subjects.Length; i++)
@@ -125,29 +132,29 @@ namespace StudentRegistry
         }
 
         string[] SeparateNamesInDifferentStrings(Student student)
-        {                     
+        {
             string[] names = new string[1];
             int j = 0;
             for (int i = 0; i < student.studentName.Length; i++)
             {
-                if (student.studentName[i]!=' ')
+                if (student.studentName[i] != ' ')
                     names[j] += student.studentName[i];
                 else
                 {
                     Array.Resize(ref names, names.Length + 1);
-                    j++;                  
-                }          
+                    j++;
+                }
             }
             return names;
         }
 
-        public bool CheckIfInAlphabeticalOrder(string[]studentOneNames,string[]studentTwoNames)
+        public bool CheckIfInAlphabeticalOrder(string[] studentOneNames, string[] studentTwoNames)
         {
             int minNumberOfNames = (studentOneNames.Length < studentOneNames.Length) ? studentOneNames.Length : studentTwoNames.Length;
             bool isAlphabetical = true;
             for (int i = 0; i < minNumberOfNames; i++)
             {
-                int j = 0;               
+                int j = 0;
                 while (j < minLengthOfNames(studentOneNames[i], studentTwoNames[i]))
                 {
                     if (studentOneNames[i][j] < studentTwoNames[i][j])
@@ -155,44 +162,44 @@ namespace StudentRegistry
                     else if (studentOneNames[i][j] > studentTwoNames[i][j])
                         return isAlphabetical = false;
                     else j++;
-                }             
+                }
             }
             return isAlphabetical;
         }
 
-        int minLengthOfNames(string nameOne,string nameTwo)
+        int minLengthOfNames(string nameOne, string nameTwo)
         {
-            return (nameOne.Length < nameTwo.Length) ? nameOne.Length : nameTwo.Length;           
+            return (nameOne.Length < nameTwo.Length) ? nameOne.Length : nameTwo.Length;
         }
 
         decimal FindSmallestGradeAverage(Student[] students)
         {
             decimal smallestGradeAverage = AverageGeneralGradeForEachStudent(students[0]);
-            for(int i = 1; i < students.Length; i++)
+            for (int i = 1; i < students.Length; i++)
                 smallestGradeAverage = (smallestGradeAverage > AverageGeneralGradeForEachStudent(students[i])) ? AverageGeneralGradeForEachStudent(students[i]) : smallestGradeAverage;
             return smallestGradeAverage;
         }
 
         Student FindStudentWithSmallestGradeAverage(Student[] students)
         {
-            for(int i = 0; i < students.Length; i++)
+            for (int i = 0; i < students.Length; i++)
                 if (AverageGeneralGradeForEachStudent(students[i]) == FindSmallestGradeAverage(students))
                     return students[i];
             return students[0];
         }
 
-        string[] FindStudentsWithACertainGradeAverage(Student[] students,decimal wantedGeneralGrade)
-        {           
-            string[] studentsWithCertainAverage =new string[0];
+        string[] FindStudentsWithACertainGradeAverage(Student[] students, decimal wantedGeneralGrade)
+        {
+            string[] studentsWithCertainAverage = new string[0];
             for (int i = 0; i < students.Length; i++)
             {
                 if (AverageGeneralGradeForEachStudent(students[i]) == wantedGeneralGrade)
                 {
                     Array.Resize(ref studentsWithCertainAverage, studentsWithCertainAverage.Length + 1);
-                    studentsWithCertainAverage[studentsWithCertainAverage.Length-1] += students[i].studentName;                   
+                    studentsWithCertainAverage[studentsWithCertainAverage.Length - 1] += students[i].studentName;
                 }
             }
-          return studentsWithCertainAverage;
+            return studentsWithCertainAverage;
         }
 
         public Student[] SortStudentsInAlphabeticalOrderUsingSelection(ref Student[] students)
@@ -211,13 +218,13 @@ namespace StudentRegistry
         private int GetPositionOfTheNextAlphabeticalName(Student[] students, int k, int i)
         {
             for (int j = i + 1; j < students.Length; j++)
-                if (CheckIfInAlphabeticalOrder(SeparateNamesInDifferentStrings(students[j]),SeparateNamesInDifferentStrings(students[k])))
+                if (CheckIfInAlphabeticalOrder(SeparateNamesInDifferentStrings(students[j]), SeparateNamesInDifferentStrings(students[k])))
                     k = j;
             return k;
         }
 
-        Student[] SwapStudents(ref Student[] students,int i,int k)
-        {           
+        Student[] SwapStudents(ref Student[] students, int i, int k)
+        {
             var temp = students[i];
             students[i].studentName = students[k].studentName;
             students[i].subjects = students[k].subjects;
@@ -226,10 +233,10 @@ namespace StudentRegistry
             return students;
         }
 
-        string BuildStringWithStudentNames(Student[]students)
+        string BuildStringWithStudentNames(Student[] students)
         {
-            string studentNames = string.Empty;           
-            for(int i = 0; i < students.Length; i++)
+            string studentNames = string.Empty;
+            for (int i = 0; i < students.Length; i++)
             {
                 studentNames += students[i].studentName;
                 studentNames += ",";
@@ -251,15 +258,34 @@ namespace StudentRegistry
             }
             return students;
         }
-       
+
         private bool CompareEachAverageGrade(bool inOrder, Student[] students, int i)
         {
-            if (AverageGeneralGradeForEachStudent(students[i]) > AverageGeneralGradeForEachStudent(students[i+1]))
+            if (AverageGeneralGradeForEachStudent(students[i]) > AverageGeneralGradeForEachStudent(students[i + 1]))
             {
                 inOrder = false;
-                SwapStudents(ref students,i, i + 1);
+                SwapStudents(ref students, i, i + 1);
             }
             return inOrder;
         }
+
+        string FindStudentWithACertainGradeAverageUsingBinarySearch(Student[] students, decimal wantedGrade)
+        {
+            students = SortStudentsBasedOnTheirAverageGradeUsingBubbleSorting(ref students);
+            int start = 0;
+            int end = students.Length - 1;
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                if (AverageGeneralGradeForEachStudent(students[mid]) == wantedGrade)
+                    return students[mid].studentName;
+                if (AverageGeneralGradeForEachStudent(students[mid]) < wantedGrade)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+            return "";
+        }
     }
+        
 }
